e88a3f6e08901cc20f0565bd109a29e161955f5d
Mateus Rodrigues <omateus2004@gmail.com>
2017-04-08 10:17:45 -0300

    Projeto final

---
+++ app/src/main/java/unifacisa/br/iveralda/MainActivity.java
@@ -0,0 +1,30 @@
+package unifacisa.br.iveralda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.botaoToin);
+       final TextView txt = (TextView) findViewById(R.id.vezes);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int vezesQuePegou =  Integer.parseInt(txt.getText().toString());
+
+                vezesQuePegou ++;
+
+                txt.setText(String.valueOf(vezesQuePegou));
+            }
+        });
+    }
+}
