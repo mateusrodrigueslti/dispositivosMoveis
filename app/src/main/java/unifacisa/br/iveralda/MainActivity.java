bcfa7ffb1010ecef5210e4a1abac7d6f098bc957
Mateus Rodrigues <omateus2004@gmail.com>
2017-04-12 14:51:49 -0300

    FCM + Realtime database

--- app/src/main/java/unifacisa/br/iveralda/MainActivity.java
+++ app/src/main/java/unifacisa/br/iveralda/MainActivity.java
@@ -1,29 +1,53 @@
 package unifacisa.br.iveralda;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        final DatabaseReference myRef = database.getReference("message");
+        final TextView txt = (TextView) findViewById(R.id.vezes);
 
         Button btn = (Button) findViewById(R.id.botaoToin);
-       final TextView txt = (TextView) findViewById(R.id.vezes);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int vezesQuePegou =  Integer.parseInt(txt.getText().toString());
-
                 vezesQuePegou ++;
-
                 txt.setText(String.valueOf(vezesQuePegou));
+                myRef.setValue(String.valueOf(vezesQuePegou));
+            }
+        });
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                txt.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w("Failed to read value.", error.toException());
             }
         });
     }
